20225de8c5de8aa31a3f1965f70291a6c480d3b0
Hans Delano <hanscau@hotmail.com>
2024-01-08 11:21:39 +0800

    add: router

---
+++ src/Routes.tsx
@@ -0,0 +1,7 @@
+import { useRoutes } from "react-router-dom";
+import Explore from "views/Explore";
+
+export default function Routes() {
+    const routes = useRoutes([{path: "/", element: <Explore />}])
+    return routes;
+}
---
+++ src/views/Explore.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Explore: React.FC = () => {
+    return (
+        <div>
+            <h1>Explore Page</h1>
+            {/* Add your explore page content here */}
+        </div>
+    );
+};
+
+export default Explore;
