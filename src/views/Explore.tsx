703864834ad735e63ffd647972b10bab49733b6f
Hans Delano <hanscau@hotmail.com>
2024-01-11 15:46:33 +0800

    refactor topicList and postList

--- src/views/Explore.tsx
+++ src/views/Explore.tsx
@@ -1,16 +1,14 @@
-import { ThumbUp } from "@mui/icons-material";
 import {
-  Avatar,
   Box,
   Grid,
   Typography,
   TypographyProps,
   useTheme,
 } from "@mui/material";
-import PostList from "components/PostList";
+import PostListItem from "components/PostListItem";
 import SearchBar from "components/SearchBar";
-import Post, { mockPosts } from "model/Post";
-import { mockTopics } from "model/Topic";
+import { mockPosts } from "model/Post";
+import Topic, { mockTopics } from "model/Topic";
 
 const Header = (props: TypographyProps) => {
   const theme = useTheme();
@@ -26,6 +24,50 @@ const Header = (props: TypographyProps) => {
   );
 };
 
+const TopicListItem = ({ topic }: { topic: Topic }) => {
+  return (
+    <Grid item xs={12} sm={6} md={4} lg={3}>
+      <Box
+        sx={{
+          background: "url('https://picsum.photos/id/20/300/200')",
+          backgroundSize: "cover",
+          height: "120px",
+          borderRadius: "6px",
+          display: "flex",
+          alignItems: "flex-end",
+          justifyContent: "flex-start",
+          position: "relative",
+        }}
+      >
+        <Box
+          sx={{
+            height: "100%",
+            width: "100%",
+            position: "absolute",
+            background:
+              "linear-gradient(185deg, rgba(109.44, 109.44, 109.44, 0) 0%, rgba(40.25, 40.36, 40.35, 0.50) 100%)",
+            borderRadius: "6px",
+          }}
+        ></Box>
+        <Box
+          display={"flex"}
+          alignItems={"flex-end"}
+          gap={"8px"}
+          m="8px 12px"
+          zIndex={5}
+        >
+          <Typography fontSize={"20px"} color={"white"} fontWeight={700}>
+            {topic.Name}
+          </Typography>
+          <Typography fontSize={"12px"} color={"white"} mb="2px">
+            {topic.PostCount} Posts
+          </Typography>
+        </Box>
+      </Box>
+    </Grid>
+  );
+};
+
 const Explore = () => {
   const theme = useTheme();
 
@@ -38,45 +80,7 @@ const Explore = () => {
       <Header mt={"24px"}>Interest</Header>
       <Grid mt="0px" container spacing={2}>
         {topics.map((topic) => (
-          <Grid item xs={12} sm={6} md={4} lg={3}>
-            <Box
-              sx={{
-                background: "url('https://picsum.photos/id/20/300/200')",
-                backgroundSize: "cover",
-                height: "120px",
-                borderRadius: "6px",
-                display: "flex",
-                alignItems: "flex-end",
-                justifyContent: "flex-start",
-                position: "relative",
-              }}
-            >
-              <Box
-                sx={{
-                  height: "100%",
-                  width: "100%",
-                  position: "absolute",
-                  background:
-                    "linear-gradient(185deg, rgba(109.44, 109.44, 109.44, 0) 0%, rgba(40.25, 40.36, 40.35, 0.50) 100%)",
-                  borderRadius: "6px",
-                }}
-              ></Box>
-              <Box
-                display={"flex"}
-                alignItems={"flex-end"}
-                gap={"8px"}
-                m="8px 12px"
-                zIndex={5}
-              >
-                <Typography fontSize={"20px"} color={"white"} fontWeight={700}>
-                  {topic.Name}
-                </Typography>
-                <Typography fontSize={"12px"} color={"white"} mb="2px">
-                  {topic.PostCount} Posts
-                </Typography>
-              </Box>
-            </Box>
-          </Grid>
+          <TopicListItem topic={topic} />
         ))}
       </Grid>
       <Header mt={"24px"} mb={"16px"}>
@@ -84,7 +88,7 @@ const Explore = () => {
       </Header>
       <Box display="flex" flexDirection="column" gap="4px">
         {posts.map((post) => (
-          <PostList post={post}></PostList>
+          <PostListItem post={post}></PostListItem>
         ))}
       </Box>
     </Box>
